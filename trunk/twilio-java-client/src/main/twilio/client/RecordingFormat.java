07b6a1ebcaa136a6c468d3feb907a843fc14050f
sean@seansullivan.com <sean@seansullivan.com@62e1f2a4-e753-11dd-80ed-bda8ba2650cd>
2009-01-26 19:39:02 +0000

    added DEFAULT

--- trunk/twilio-java-client/src/main/twilio/client/RecordingFormat.java
+++ trunk/twilio-java-client/src/main/twilio/client/RecordingFormat.java
@@ -3,9 +3,12 @@ package twilio.client;
 
 public enum RecordingFormat
 {
+	
 	WAV("audio/x-wav", ".wav"), 
 	MP3("audio/mpeg", ".mp3");
 
+	public static final RecordingFormat DEFAULT = WAV;
+	
 	private String mime;
 	private String fileExtension;
 	
