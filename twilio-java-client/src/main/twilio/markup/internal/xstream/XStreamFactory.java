97b6e6fa1cd3e56504a04ab806cb0307cd61310b
sean <sean@seansullivan.com>
2009-01-28 11:21:18 +0000

    added javadoc

--- twilio-java-client/src/main/twilio/markup/internal/xstream/XStreamFactory.java
+++ twilio-java-client/src/main/twilio/markup/internal/xstream/XStreamFactory.java
@@ -3,8 +3,12 @@ package twilio.markup.internal.xstream;
 
 import com.thoughtworks.xstream.XStream;
 
-import twilio.markup.*;
 
+/**
+ * 
+ * XStream factory for twilio.markup package
+ *
+ */
 public class XStreamFactory
 {
 	public static XStream createXStream()
