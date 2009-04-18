d0a10b857d5851a1b38192d194bfa763ad0bac0d
sean@seansullivan.com <sean@seansullivan.com@62e1f2a4-e753-11dd-80ed-bda8ba2650cd>
2009-04-18 21:31:57 +0000

    setContentType

--- trunk/twilio-java-client/src/main/twilio/servlet/TwilioServlet.java
+++ trunk/twilio-java-client/src/main/twilio/servlet/TwilioServlet.java
@@ -20,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 public abstract class TwilioServlet extends HttpServlet
 {
 	private static final long serialVersionUID = 1L;
+	public static final String TWIML_CONTENT_TYPE = "text/xml";
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp)
@@ -29,6 +30,8 @@ public abstract class TwilioServlet extends HttpServlet
 	
 	protected void doTwilioRequest(TwilioRequest req, HttpServletResponse resp)
 	{
+		resp.setContentType(TWIML_CONTENT_TYPE);
+		
 		if (req.isDialCallback())
 		{
 			onDialCallback(req, resp);
