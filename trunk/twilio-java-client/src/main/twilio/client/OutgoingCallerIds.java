34c9e9ff09592a7c64aa216b517ba978353e4b89
sean@seansullivan.com <sean@seansullivan.com@62e1f2a4-e753-11dd-80ed-bda8ba2650cd>
2009-07-01 02:15:07 +0000

    fixed superclass.  Added method:  getPhoneNumbers

--- trunk/twilio-java-client/src/main/twilio/client/OutgoingCallerIds.java
+++ trunk/twilio-java-client/src/main/twilio/client/OutgoingCallerIds.java
@@ -1,7 +1,21 @@
 
 package twilio.client;
 
-public class OutgoingCallerIds extends TwilioList<OutgoingCallerIds>
+import java.util.ArrayList;
+import java.util.List;
+
+public class OutgoingCallerIds extends TwilioList<OutgoingCallerId>
 {
 
+	public List<String> getPhoneNumbers()
+	{
+		List<String> phoneNumbers = new ArrayList<String>();
+		
+		for (OutgoingCallerId outgoing : this)
+		{
+			phoneNumbers.add(outgoing.getPhoneNumber());
+		}
+		
+		return phoneNumbers;
+	}
 }
