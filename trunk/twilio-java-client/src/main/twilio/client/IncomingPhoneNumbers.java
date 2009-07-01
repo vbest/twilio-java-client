1f642974974d1a94fe25e729f00aa56ea7b67c65
sean@seansullivan.com <sean@seansullivan.com@62e1f2a4-e753-11dd-80ed-bda8ba2650cd>
2009-07-01 02:15:44 +0000

    added method:  getPhoneNumbers()

--- trunk/twilio-java-client/src/main/twilio/client/IncomingPhoneNumbers.java
+++ trunk/twilio-java-client/src/main/twilio/client/IncomingPhoneNumbers.java
@@ -1,7 +1,20 @@
 
 package twilio.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IncomingPhoneNumbers extends TwilioList<IncomingPhoneNumber>
 {
-
+	public List<String> getPhoneNumbers()
+	{
+		List<String> phoneNumbers = new ArrayList<String>();
+		
+		for (IncomingPhoneNumber incoming : this)
+		{
+			phoneNumbers.add(incoming.getPhoneNumber());
+		}
+		
+		return phoneNumbers;
+	}
 }
