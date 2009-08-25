7ff9047216f5dfb3bda3a233966da41cefba56e2
sean <sean@seansullivan.com>
2009-08-25 04:51:04 +0000

    added two methods.

--- twilio-java-client/src/main/twilio/client/Calls.java
+++ twilio-java-client/src/main/twilio/client/Calls.java
@@ -1,7 +1,33 @@
 
 package twilio.client;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Calls extends TwilioList<Call>
 {
-
+	public Collection<String> getCallers()
+	{
+		Set<String> callers = new HashSet<String>();
+		
+		for (Call c : this)
+		{
+			callers.add(c.getCaller());
+		}
+		
+		return callers;
+	}
+	
+	public Collection<String> getCalledNumbers()
+	{
+		Set<String> called = new HashSet<String>();
+		
+		for (Call c : this)
+		{
+			called.add(c.getCalled());
+		}
+		
+		return called;
+	}
 }
